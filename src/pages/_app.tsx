6119b60af46c3a9afb8943ed01e632b56396fd27
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-01-01 17:32:43 -0300

    feat: config custom theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { Header } from '../components'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Header />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
