10695556c76a68be3ae2317bd460c77222598583
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-01-14 20:10:35 -0300

    feat: make travel type more responsive

--- src/components/travel-types/travel-type.tsx
+++ src/components/travel-types/travel-type.tsx
@@ -1,12 +1,28 @@
-import { Image, Text, WrapItem } from '@chakra-ui/react'
+import { Icon, Image, Text, useBreakpointValue, WrapItem } from '@chakra-ui/react'
+import { FaCircle } from 'react-icons/fa'
 
 import { TravelItem } from './types'
 
 export function TravelType({ icon, title }: TravelItem) {
+  const isInSmallScreen = !useBreakpointValue({
+    sm: true,
+  })
+
   return (
-    <WrapItem flexDir='column' alignItems='center'>
-      <Image boxSize='20' src={icon} alt={title} />
-      <Text mt='4' fontWeight='bold'>
+    <WrapItem 
+      flexDir={['row', 'column']} 
+      alignItems='center' 
+      justifyContent='center'
+      _last={{
+        gridColumnStart: ['1', 'auto'],
+        gridColumnEnd: ['3', 'auto']  
+      }}
+    >
+      {isInSmallScreen 
+        ? <Icon as={FaCircle} size='4' color='yellow.500' /> 
+        : <Image boxSize='20' src={icon} alt={title} />
+      }
+      <Text ml={['2', 0]} mt={[0, '4']} fontWeight='bold'>
         {title}
       </Text>
     </WrapItem>
